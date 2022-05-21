54b7ca6c854ce72f1b1126756e488d4f8e9af9b2
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2022-05-21 18:55:07 +0530

    Update archpkgbuild.sh

--- archpkgbuild.sh
+++ archpkgbuild.sh
@@ -1,49 +1,73 @@
 
 cd appmotor
+chmod 700 *
 makepkg;cd ../
 cd qt-plugins
+chmod 700 *
 makepkg;cd ../
 cd gtk-themes
+chmod 700 *
 makepkg;cd ../
 cd screenlocker
+chmod 700 *
 makepkg;cd ../
 cd screenshot
+chmod 700 *
 makepkg;cd ../
 cd calculator
+chmod 700 *
 makepkg;cd ../
 cd kwin-plugins
+chmod 700 *
 makepkg;cd ../
 cd statusbar
+chmod 700 *
 makepkg;cd ../
 cd core
+chmod 700 *
 makepkg;cd ../
 cd launcher
+chmod 700 *
 makepkg;cd ../
 cd statusbar-test
+chmod 700 *
 makepkg;cd ../
 cd daemon
+chmod 700 *
 makepkg;cd ../
 cd libmatsya
+chmod 700 *
 makepkg;cd ../
 cd terminal
+chmod 700 *
 makepkg;cd ../
 cd debinstaller
+chmod 700 *
 makepkg;cd ../
 cd matsya-settings
+chmod 700 *
 makepkg;cd ../
 cd texteditor
+chmod 700 *
 makepkg;cd ../
 cd dock
+chmod 700 *
 makepkg;cd ../
 cd matsyaui
+chmod 700 *
 makepkg;cd ../
 cd videoplayer
+chmod 700 *
 makepkg;cd ../
 cd filemanager
+chmod 700 *
 makepkg;cd ../
 cd matsya-updater
+chmod 700 *
 makepkg;cd ../
 cd wallpappers
+chmod 700 *
 makepkg;cd ../
 cd icons
+chmod 700 *
 makepkg;cd ../
